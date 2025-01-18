d3c98a1f01aaff881e3416712163f31c1b03b439
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-01-18 12:52:59 +0000

    (feat: JS) add BS Card component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,37 @@
+
+import storeItems from "../data/items.json";
+import Card from "react-bootstrap/Card";
+
 export default function Store() {
-    return (
-        <div>Store department</div>
-    )
+  return (
+    <div className="d-flex flex-wrap gap-3">
+      {storeItems.map((storeItem) => (
+        <Card key={storeItem.id}>
+          <Card.Header>{storeItem.name}</Card.Header>
+          <Card.Body>
+            <Card.Title>{storeItem.price}</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
 }
+// {
+//     "id": 1,
+//     "name": "Book",
+//     "price": 10.99,
+//     "imgUrl": "/imgs/book.jpg"
+//   },
+
+// <Card>
+// <Card.Header>Featured</Card.Header>
+// <Card.Body>
+//   <Card.Title>Special title treatment</Card.Title>
+//   <Card.Text>
+//     With supporting text below as a natural lead-in to additional content.
+//   </Card.Text>
+// </Card.Body>
+// </Card>
