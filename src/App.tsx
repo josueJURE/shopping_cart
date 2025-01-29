d63d97b5e40902fe44ae6a691f6ef29503353982
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-01-29 15:35:56 +0000

    Update ESLint configuration and fix typo in Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,25 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Store from "./pages/Store";
 import NotFound from "./pages/NotFount";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ShoppingCartProvider from "./context/UseShoppingCart";
-
 
 function App() {
   return (
     <>
-    <ShoppingCartProvider>
-    <Navbar />
-      <Container className="mb-4 bg-primary" >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="*" element={<NotFound />} />{" "}
-          {/* Catch-all route for undefined paths */}
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
-     
+      <ShoppingCartProvider>
+        <Navbar />
+        <Container className="mb-4 bg-primary">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="*" element={<NotFound />} />{" "}
+            {/* Catch-all route for undefined paths */}
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
     </>
   );
 }
