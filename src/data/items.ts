e29c07596f3c27e11c426a4cedaaa65270651429
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-01-29 15:46:21 +0000

    change to ts file

---
+++ src/data/items.ts
@@ -0,0 +1,28 @@
+ const data = [
+  {
+    id: 1,
+    name: "Book",
+    price: 10.99,
+    imgUrl: "/images/book.jpg",
+  },
+  {
+    id: 2,
+    name: "Computer",
+    price: 1199,
+    imgUrl: "/images/computer.jpg",
+  },
+  {
+    id: 3,
+    name: "Banana",
+    price: 1.05,
+    imgUrl: "/images/banana.jpg",
+  },
+  {
+    id: 4,
+    name: "Car",
+    price: 14000,
+    imgUrl: "/images/car.jpg",
+  },
+] as const
+
+export default data
