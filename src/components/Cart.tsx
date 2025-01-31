026c073c28849b5f6315b0b884945a958fde4205
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-01-31 12:44:09 +0000

    chore: Refactor Cart component and improve code formatting

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,34 +1,46 @@
-import { useShoppingCart } from "../context/UseShoppingCart"
-import { Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/UseShoppingCart";
+import { Button, Stack } from "react-bootstrap";
 
 import data from "../data/items";
-
+import formatCurrency from "../utilities/formatCurrency";
 
 type CartTypesProps = {
-    id : number
-    quantity: number
-}
+  id: number;
+  quantity: number;
+};
 
-export default function Cart({id, quantity}: CartTypesProps) {
-    const {removeFromCart} = useShoppingCart()
-    const item = data.find(i => i.id === id)
-    if (item == null) return null
+export default function Cart({ id, quantity }: CartTypesProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = data.find((i) => i.id === id);
+  if (item == null) return null;
 
-    return (
-     
-        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
-            <img src={item.imgUrl} style={{width: "125px", height:"75px", objectFit: "cover"}}/>
-            <div className="me-auto">
-            <div>{item.name} 
-                {quantity > 1 && <span className="text-muted">{quantity}x</span>}
+  return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price * quantity)}
+        </div>
 
-
-            </div>
-
-            </div>
-         
-
-        </Stack>
-    )
- 
+        <Button
+          variant="outline-danger"
+          onClick={() => removeFromCart(item.id)}
+        >Remove Items</Button>
+      </div>
+    </Stack>
+  );
 }
