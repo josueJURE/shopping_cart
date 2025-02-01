c164470675e522ca2fe67ae888270be996428a86
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2025-02-01 11:47:44 +0000

    refactor ShoppingCart component and improve code formatting

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,15 +1,15 @@
-
-import { Offcanvas, Stack} from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/UseShoppingCart";
 import Cart from "./Cart";
+import formatCurrency from "../utilities/formatCurrency";
+import StoreItems from "../data/items";
 
 type ShoppingCartProps = {
-    isOpen: boolean
-}
+  isOpen: boolean;
+};
 
-export default function ShoppingCart({isOpen} : ShoppingCartProps ) {
-
-  const { closeCart, cartItems} = useShoppingCart();
+export default function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement="end">
       <Offcanvas.Header closeButton>
@@ -17,11 +17,18 @@ export default function ShoppingCart({isOpen} : ShoppingCartProps ) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <Stack gap={3}>
-           {cartItems.map(item => 
+          {cartItems.map((item) => (
             <Cart key={item.id} {...item}></Cart>
-           )}
-          
-
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = StoreItems.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
